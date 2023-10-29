d9edace26bd47fedebf2baf5e6a653c277de3952
Du Lam Minh <lmdu@gmail.com>
2023-10-29 10:19:03 +0000

    tutorial-3: Update code on tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from "react-router-dom"
 
 //pages
 import AboutPage from "./pages/About";
 import HomePage from "./pages/Home";
+
+//layouts
+import RootLayout from "./layouts/RouteLayOut";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<HomePage />}/>
+      <Route path="/about" element={<AboutPage />}/>
+    </Route>
+  )
+)
+
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>React Router</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<HomePage />}/>
-          <Route path="/about" element={<AboutPage />}/>
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   );
 }
 
