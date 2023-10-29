07baaa3ea13648fb8cee6678260c15cc063979fd
Du Lam Minh <lmdu@gmail.com>
2023-10-29 08:02:10 +0000

    tutorial-2: remove outer div

--- src/App.js
+++ src/App.js
@@ -5,23 +5,21 @@ import AboutPage from "./pages/About";
 import HomePage from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <header>
-          <nav>
-            <h1>React Router</h1>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-          </nav>
-        </header>
-        <main>
-          <Routes>
-            <Route index element={<HomePage/>}/>
-            <Route path="/about" element={<AboutPage/>}/>
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>React Router</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<HomePage />}/>
+          <Route path="/about" element={<AboutPage />}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
