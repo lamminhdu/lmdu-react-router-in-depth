ce933c78979c03e53812a1ddd144b4735ff21573
Du Lam Minh <lmdu@gmail.com>
2023-10-30 15:07:29 +0000

    tutorial-4: Update code on tutorial

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import {
   createBrowserRouter, 
   createRoutesFromElements, 
   Route, 
-  RouterProvider 
+  RouterProvider
 } from "react-router-dom"
 
 //pages
 import AboutPage from "./pages/About";
 import HomePage from "./pages/Home";
+import Contact from "./pages/help/Contact";
+import HelpLayout from "./layouts/HelpLayout";
 
 //layouts
 import RootLayout from "./layouts/RouteLayOut";
+import FaqPage from "./pages/help/Faq";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<HomePage />}/>
       <Route path="/about" element={<AboutPage />}/>
+      <Route path="/help" element={<HelpLayout />}>
+        <Route path="contact" element={<Contact />}/>
+        <Route path="faq" element={<FaqPage />}/>
+      </Route>
     </Route>
   )
 )
