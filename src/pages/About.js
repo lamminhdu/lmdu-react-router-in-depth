445a4995919dafd93ea3d798633c181f6f640e09
Du Lam Minh <lamminhdu97@gmail.com>
2023-11-02 16:31:34 +0000

    tutorial-11: update code on tutorial

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,20 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom"
+
 export default function AboutPage() {
+    const [user, setUser] = useState("Andrew")
+
+    if (!user) {
+        return (
+            <Navigate to="/" replace={true} />
+        )
+    }
+    
     return (
         <div className="about">
             <h2>About</h2>
             <p>About content</p>
+            <button onClick={() => setUser(null)}>Logout</button>
         </div>
     )
 }
