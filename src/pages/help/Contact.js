9398b0e38dc9bcecf4c6ba6faf1f7d2e1092e5f6
Du Lam Minh <lamminhdu97@gmail.com>
2023-11-05 11:10:13 +0000

    tutorial-10: update code on tutorial

--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -1,8 +1,12 @@
+import { Form, redirect, useActionData } from "react-router-dom";
+
 export default function ContactPage() {
+    const data = useActionData()
+    console.log(data)
     return (
       <div className="contact">
         <h3>Contact Us</h3>
-        <form>
+        <Form method="post" action="/help/contact">
           <label>
             <span>Your email:</span>
             <input type="email" name="email" required />
@@ -12,7 +16,23 @@ export default function ContactPage() {
             <textarea name="message" required></textarea>
           </label>
           <button>Submit</button>
-        </form>
+          {data && data.error && <p>{data.error}</p>}
+        </Form>
       </div>
     )
+  }
+
+  export const contactAction = async ( { request } ) => {
+    const data = await request.formData()
+    const submision = {
+      email: data.get("email"),
+      message: data.get("message")
+    }
+    
+    console.log(submision)
+    if (submision.message.length < 10) {
+      return {error: "message under 10 chars"}
+    }
+    
+    return redirect("/")
   }
